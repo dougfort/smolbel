0ea2b53cc135f10747353b671fdba569ecb1e629
dougfort <doug.fort@gmail.com>
2022-02-01 15:10:27 -0500

    start REPL with  rustyline

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{anyhow, Error};
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
+fn main() -> Result<(), Error> {
+    let mut rl = Editor::<()>::new();
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    };
+
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                return Err(anyhow!("Error from readline: {:?}", err));
+            }
+        }
+    }
+    rl.save_history("history.txt").unwrap();
+
+    Ok(())
 }
